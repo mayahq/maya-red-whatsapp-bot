85a80d652075e7e75747de2c065d0375a1f000ce
shubham-root <shubham.root@gmail.com>
2020-09-28 04:51:02 +0530

    create group node added

---
+++ createGroup.js
@@ -0,0 +1,112 @@
+module.exports = function (RED) {
+    'use strict'
+
+    function WhatsappBotCreateGroup(config) {
+        RED.nodes.createNode(this, config)
+
+        const node = this
+        node.name = config.name
+
+        console.log(node.name);
+
+        const SOCKETS_STATE = {
+            OPENING: 'info',
+            PAIRING: 'info',
+            UNPAIRED: 'info',
+            UNPAIRED_IDLE: 'info',
+            CONNECTED: 'success',
+            TIMEOUT: 'error',
+            CONFLICT: 'error',
+            UNLAUNCHED: 'error',
+            PROXYBLOCK: 'error',
+            TOS_BLOCK: 'error',
+            SMB_TOS_BLOCK: 'error',
+            DEPRECATED_VERSION: 'error'
+        }
+
+        const clientNode = RED.nodes.getNode(config.client)
+
+        function registerEvents() {
+            clientNode.on('stateChange', onStateChange.bind(node))
+            clientNode.on('clientEvent', onClientEvent.bind(node))
+        }
+
+        function onStateChange(socketState) {
+            setStatus(SOCKETS_STATE[socketState], 'Socket: ' + socketState)
+        }
+
+        function onClientEvent(eventName, ...args) {
+            node.send({ topic: eventName, payload: args })
+        }
+
+        function onChatEvent(event, chatId, ...args) {
+            node.send({ topic: event, chatId: chatId, args: args })
+        }
+
+        if (clientNode) {
+            clientNode.register(node)
+
+            setStatus('warning', 'Authenticating...')
+
+            clientNode.on('qrCode', function (qrCode) {
+                node.send({ topic: 'qrCode', payload: [qrCode] })
+            })
+
+            clientNode.on('ready', function (client) {
+                setStatus('success', 'Connected')
+
+                node.client = client
+            })
+
+            registerEvents()
+        }
+
+        node.on('input', function (msg) {
+
+            if (msg.topic === 'restart') {
+                setStatus('warning', 'Authenticating...')
+                clientNode.restart()
+                    .then(() => node.send({ topic: msg.topic, origin: msg, payload: [true] }))
+                    .catch((err) => node.error('Error while restarting client ' + err.message))
+                return
+            }
+
+            if (!node.client) {
+                setStatus('error', 'Client not connected')
+                return
+            }
+
+
+            if (msg.groupName.length > 0 && msg.participants.length > 0) {
+                node.client["createGroup"](msg.groupName, msg.participants).then((...args) => {
+                    node.send({
+                        topic: "createGroup",
+                        payload: args,
+                        origin: msg
+                    })
+                }).catch(err => {
+                    node.error('Requested api "' + msg.topic + '" ' + err.message)
+                })
+            } else {
+                node.send({
+                    topic: "createGroup",
+                    payload: "Group name and a minimum of 1 participant is required",
+                    origin: msg
+                });
+            }
+        })
+
+        // Set node status
+        function setStatus(type, message) {
+            const types = { info: 'blue', error: 'red', warning: 'yellow', success: 'green' }
+
+            node.status({
+                fill: types[type] || 'grey',
+                shape: 'dot',
+                text: message
+            })
+        }
+    }
+
+    RED.nodes.registerType('create-group', WhatsappBotCreateGroup)
+}
